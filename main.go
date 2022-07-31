789507a9c0f4b6605b4e4a94e3407ab19c373af4
Harivansh Thakur <harivanshthakur8@gmail.com>
2022-07-31 23:58:55 +0530

    added the root path

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Printf("Initialized :)")
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Default message :)",
+		})
+	})
+
+	router.Run("localhost:8080")
+
 }
